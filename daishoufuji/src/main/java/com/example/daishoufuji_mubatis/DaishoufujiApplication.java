8298dc8e2e457c284ba6101da46d0b0bfa378f79
10-fujita <fujitahironari1019@gmail.com>
2025-12-24 23:39:06 +0900

    新規作成

---
+++ daishoufuji/src/main/java/com/example/daishoufuji_mubatis/DaishoufujiApplication.java
@@ -0,0 +1,13 @@
+package com.example.daishoufuji_mubatis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaishoufujiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaishoufujiApplication.class, args);
+	}
+
+}
---
+++ daishoufuji/src/test/java/com/example/daishoufuji_mubatis/DaishoufujiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.daishoufuji_mubatis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaishoufujiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
