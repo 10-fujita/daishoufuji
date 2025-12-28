112304e1352facf0f02217701b32bf4eaba9d994
10-fujita <fujitahironari1019@gmail.com>
2025-12-28 23:34:19 +0900

    ログインまでの画面遷移

---
+++ daishoufuji/src/main/java/com/example/daishoufuji_mubatis/controller/AuthController.java
@@ -0,0 +1,39 @@
+package com.example.daishoufuji_mubatis.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/auth")
+public class AuthController {
+	//welcomページ表示
+@GetMapping("/")
+public String toppage() {
+	return "auth/welcome";
+}
+//トップページへ遷移
+@GetMapping("/top")
+public String top() {
+	return "auth/top";
+}
+//新規会員登録へ遷移
+@GetMapping("/regist")
+public String regist() {
+	return "auth/regist";
+}
+@GetMapping("/reset")
+public String reset() {
+	return "auth/reset";
+}
+@PostMapping("/regist")
+public String registSubmit() {
+	return "auth/reCheck";
+}
+@GetMapping("/correct")
+public String correct() {
+	return "auth/regist";
+}
+
+}
