f7827599c8a54463330c6cfcb23c5393b9355397
10-fujita <fujitahironari1019@gmail.com>
2026-01-09 11:11:34 +0900

    login処理のためのsecurityフォルダ関係のファイル追加

---
+++ daishoufuji/src/main/java/com/example/daishoufuji_mubatis/security/UserDetailsServiceImpl.java
@@ -0,0 +1,45 @@
+package com.example.springlesson.security;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.example.springlesson.entity.User;
+import com.example.springlesson.repository.UserRepository;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+  private final UserRepository userRepository;
+  
+  // コンストラクターインジェクション
+  public UserDetailsServiceImpl(UserRepository userRepository) {
+    this.userRepository = userRepository;
+  }
+
+
+
+  @Override
+  public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+    try {
+      User user = userRepository.findByEmail(email);
+      if(user == null) {
+        throw new UsernameNotFoundException("ユーザーが見つかりません：" + email);
+      }
+      // 権限の設定
+      Collection<GrantedAuthority> authorities = new ArrayList<>();
+      if(user.getRole() != null) {
+          authorities.add(new SimpleGrantedAuthority("ROLE_" + user.getRole().getName()));
+      }
+      return new UserDetailsImpl(user, authorities);
+    }catch(Exception e) {
+      throw new UsernameNotFoundException("ユーザーが見つかりませんでした。");
+    }
+  }
+  
+}
